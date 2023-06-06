93575f89e7139e93765e7c773474f9b98d811b59
enzomf <enzomfigueiredo11@gmail.com>
2023-06-06 08:59:50 -0300

    adicionada constante de videos

---
+++ constants/videos.js
@@ -0,0 +1,22 @@
+const videos = [
+  {
+    id: 1,
+    titulo: "Como fazer login ? ",
+    descricao: "Description 1",
+    link: "https://www.youtube.com/embed/1",
+  },
+  {
+    id: 2,
+    titulo: "Como recuperar senha",
+    descricao: "Description 2",
+    link: "https://www.youtube.com/embed/2",
+  },
+  {
+    id: 3,
+    titulo: "Como adicionar clientes",
+    descricao: "Description 3",
+    link: "https://www.youtube.com/embed/3",
+  },
+];
+
+export { videos };
