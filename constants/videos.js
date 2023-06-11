06120fd7c9e2479da725002aa618997933b977f3
Felipe Pereira Moura dos santos <111398226+FelipePdev@users.noreply.github.com>
2023-06-11 11:52:42 -0300

    Add files via upload
    adicionado os videos no youtube e na pagina de suporte

--- constants/videos.js
+++ constants/videos.js
@@ -1,27 +1,51 @@
 const videos = [
   {
     id: 1,
-    titulo: "Como fazer login ? ",
-    descricao: "Description 1",
-    link: "https://www.youtube.com/embed/1",
+    titulo: "Visão geral",
+    descricao: "Conhecendo as funcionalidades do software",
+    link: "https://www.youtube.com/watch?v=HUppEV3A2Eg",
   },
   {
     id: 2,
-    titulo: "Como recuperar senha",
-    descricao: "Description 2",
-    link: "https://www.youtube.com/embed/2",
+    titulo: "Como acessar o software",
+    descricao: "aprendendo a realizar acesso ao software",
+    link: "https://youtu.be/7EtVOfnO2hw",
   },
   {
     id: 3,
-    titulo: "Como adicionar clientes",
-    descricao: "Description 3",
-    link: "https://www.youtube.com/embed/3",
+    titulo: "Como alterar a senha de  acesso",
+    descricao: "Aprendendo a alterar a senha de acesso do software",
+    link: "https://youtu.be/VMM6diFbZo4"
   },
   {
     id: 4,
-    titulo: "Como alterar a senha",
-    descricao: "Description 4",
-    link: "https://www.youtube.com/embed/4",
+    titulo: "Como recuperar a senha de acesso do software",
+    descricao: "Aprendendo a recuperar a senha de acesso do software",
+    link: "https://youtu.be/Zmje29Z6O_g",
+  },
+  {
+    id: 5,
+    titulo: "Estatísticas do software",
+    descricao: "Aprendendo sobre as estatíticas do software",
+    link: "https://youtu.be/_OuTiEokhnI",
+  },
+  {
+    id: 6,
+    titulo: "Clientes do software",
+    descricao: "Aprendendo sobre os clientes do software",
+    link: "https://youtu.be/i1QcF5f7lqg",
+  },
+  {
+    id: 7,
+    titulo: "Colaboradores no software",
+    descricao: "Aprendendo sobre cadastro de colaboradores no software",
+    link: "https://youtu.be/ouyqmNWlQc0",
+  },
+  {
+    id: 8,
+    titulo: "Registro das atividades no software",
+    descricao: "Aprendendo sobre os registros das atividades no software",
+    link: "https://youtu.be/ThVeo13WNgY",
   },
 ];
 
